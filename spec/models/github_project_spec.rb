81f70d596106b31e4089b267a7a5cc494453d227
Andrew Hyder <hyd415@gmail.com>
2016-06-25 23:41:39 -0700

    Better gihtub update tests

--- spec/models/github_project_spec.rb
+++ spec/models/github_project_spec.rb
@@ -1,21 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe GithubProject, type: :model do
+  before do
+    @gh = GithubProject.new("https://www.github.com/ondrae/gitcollections")
+    allow(@gh).to receive(:data).and_return({name: "UPDATED NAME", description: "UPDATED DESCRIPTION"})
+  end
 
   describe "a new GithubProject" do
     it "it validates" do
-      gh = GithubProject.new("https://www.github.com/ondrae/gitcollections")
-      expect(gh).to be_valid
+      expect(@gh).to be_valid
     end
 
     it "it requires github urls" do
-      gh = GithubProject.new("https://www.google.com/ondrae/gitcollections")
-      expect(gh).to_not be_valid
+      @gh = GithubProject.new("https://www.google.com/ondrae/gitcollections")
+      expect(@gh).to_not be_valid
+      @gh = GithubProject.new("https://www.google.com")
+      expect(@gh).to_not be_valid
     end
 
     it "gets the right repo_path" do
-      gh = GithubProject.new("https://www.github.com/ondrae/gitcollections")
-      expect(gh.repo_path).to eq("ondrae/gitcollections")
+      expect(@gh.repo_path).to eq("ondrae/gitcollections")
+    end
+
+    it "gets the GithubProject name and description" do
+      expect(@gh.name).to eq("UPDATED NAME")
+      expect(@gh.description).to eq("UPDATED DESCRIPTION")
     end
   end
 
