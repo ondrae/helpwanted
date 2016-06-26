5700151fcd9449b44aa5180fdc60f504cf75673c
Andrew Hyder <hyd415@gmail.com>
2016-06-26 00:49:10 -0700

    Validate issues

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,4 @@
 class Issue < ActiveRecord::Base
   belongs_to :project
+  validates :title, :url, presence: true
 end
--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Issue, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "new issues" do
+    it "validates" do
+      issue = create :issue
+      expect(issue).to be_valid
+    end
+  end
+
 end
