2d2f288d9ffcc0f2bdd12b5e99b2fea137d7a871
Andrew Hyder <hyd415@gmail.com>
2016-07-14 15:09:26 -0700

    Tests for updating projects

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -5,6 +5,32 @@ RSpec.describe Project, type: :model do
     @project = create :project
   end
 
+  describe "#update_project" do
+    let(:gh_project){ double(GithubProject, name: "UPDATED NAME", description: "UPDATED DESCRIPTION") }
+    before do
+      allow(GithubProject).to receive(:new).and_return( gh_project )
+    end
+    it "updates the project" do
+      @project.update_project
+
+      expect(@project.name).to eq "UPDATED NAME"
+      expect(@project.description).to eq "UPDATED DESCRIPTION"
+    end
+  end
+
+  describe "#update_issues" do
+    let(:gh_project){ double(GithubProject, issues:[double(Issue, title: "UPDATED TITLE", html_url: "https://api.github.com/repos/TEST_GITHUB_ACCOUNT/TEST_PROJECT")] ) }
+    before do
+      allow(GithubProject).to receive(:new).and_return( gh_project )
+      create :issue, project: @project
+    end
+    it "updates the project" do
+      @project.update_issues
+
+      expect(@project.issues.first.title).to eq "UPDATED TITLE"
+    end
+  end
+
   describe "#delete" do
     before do
       3.times do
