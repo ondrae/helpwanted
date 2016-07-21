adc91aed1d8da56fdda51ef3cc024758524d40f8
Andrew Hyder <hyd415@gmail.com>
2016-07-21 10:49:01 -0700

    Adding github_org model

---
+++ app/models/github_organization.rb
@@ -0,0 +1,25 @@
+require 'uri'
+
+class GithubOrganization
+  include ActiveModel::Validations
+  validates_format_of :url, :with => URI.regexp
+  validates_format_of :url, :with => /github\.com\/[a-zA-Z\-_0-9]+/
+
+  attr_accessor :url
+
+  def initialize(url)
+    @url = url
+    @github_api = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"], auto_paginate: true)
+    /github\.com\/(?<org_name>[a-zA-Z\-_0-9]+)?/ =~ @url
+    @org_name = org_name
+  end
+
+  def repos
+    @repos ||= @github_api.org_repos @org_name
+  end
+
+  def org_name
+    @org_name
+  end
+
+end
