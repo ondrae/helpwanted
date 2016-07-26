3647e7348a948348b7c040a02a80fc509e092149
Andrew Hyder <hyd415@gmail.com>
2016-07-25 22:37:52 -0700

    Bootstrap 3, Jquery 3, Rails Jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery-2.2.4.min.js
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
