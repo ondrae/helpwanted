24d1d0df58f04091eec1422a663813d60f0a5d86
Andrew Hyder <hyd415@gmail.com>
2016-12-23 09:59:49 -0800

    clean up

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   match "/delayed_job" => DelayedJobWeb, :anchor => false, via: [:get, :post]
 
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
-  resources :users, path: "people" do
+  resources :users do
     member do
       put "update_from_github"
     end
