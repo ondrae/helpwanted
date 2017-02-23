383dd75d93079af95263983d108504318c17f676
Andrew Hyder <hyd415@gmail.com>
2017-02-23 09:52:00 -0800

    delete your account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def after_sign_in_path_for(resource)
+    user_path current_user
+  end
+
   def index
     @collections = Collection.all
     if params[:search]
