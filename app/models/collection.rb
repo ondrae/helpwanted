e0ad4899077569a3cb521e58fd8181b25b39d909
Andrew Hyder <hyd415@gmail.com>
2017-02-22 22:40:46 -0800

    do we want all of orgs projects to always be included?

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -17,9 +17,10 @@ class Collection < ActiveRecord::Base
 
   def update_projects
     puts "Updating #{self.name}'s projects"
-    organizations.map do |organization|
-      organization.update_projects
-    end
+    # do we want all of orgs projects to always be included?
+    # organizations.map do |organization|
+    #   organization.update_projects
+    # end
     projects.map do |project|
       project.update_project
       project.update_issues
