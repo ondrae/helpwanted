8bb8771847b914056cb3c4eaf73d7d7f84af3bb0
Andrew Hyder <hyd415@gmail.com>
2017-04-26 16:36:35 -0700

    dont trip over bad github orgs

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -4,26 +4,30 @@ class Organization < ActiveRecord::Base
   validates :collection_id, presence: true
 
   def get_new_projects
-    puts "Asking #{url} for new projects"
+    begin
+      puts "Asking #{url} for new projects"
 
-    existing_urls = collection.projects.pluck(:url)
+      existing_urls = collection.projects.pluck(:url)
 
-    GithubOrganization.new(url).projects.each do |gh_project|
-      unless existing_urls.include? gh_project.html_url
-        gh_project_params = {
-          name: gh_project.name,
-          description: gh_project.description,
-          url: gh_project.html_url,
-          github_updated_at: gh_project.pushed_at,
-          owner_login: gh_project.owner.login,
-          owner_avatar_url: gh_project.owner.avatar_url,
-          collection_id: collection_id
-        }
-        project = Project.create!(gh_project_params)
-        if project.valid?
-          project.update_issues
+      GithubOrganization.new(url).projects.each do |gh_project|
+        unless existing_urls.include? gh_project.html_url
+          gh_project_params = {
+            name: gh_project.name,
+            description: gh_project.description,
+            url: gh_project.html_url,
+            github_updated_at: gh_project.pushed_at,
+            owner_login: gh_project.owner.login,
+            owner_avatar_url: gh_project.owner.avatar_url,
+            collection_id: collection_id
+          }
+          project = Project.create!(gh_project_params)
+          if project.valid?
+            project.update_issues
+          end
         end
       end
+    rescue
+      puts "#{url} doesn't exist anymore."
     end
   end
 
