9d7790407ace81be38a04bb1ab0192eb26450e3f
Andrew Hyder <hyd415@gmail.com>
2017-08-17 22:29:05 -0700

    Projects and orgs are alphabetical

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,6 +3,8 @@ class Organization < ActiveRecord::Base
   validates :url, presence: true, uniqueness: { scope: :collection, message: "can only add an organization once per collection" }
   validates_format_of :url, with: /github\.com\/[a-zA-Z\-_0-9]+/, on: :create
 
+  default_scope -> { order(:url) }
+
   def name
     url.split("github.com/").last
   end
--- app/models/project.rb
+++ app/models/project.rb
@@ -4,6 +4,9 @@ class Project < ActiveRecord::Base
   validates :url, presence: true, uniqueness: { scope: :collection, message: "can only add a project once per collection" }
   validates_format_of :url, with: /github\.com\/[a-zA-Z\-_0-9]+\/[a-zA-Z\-_0-9\.]+\/?/, on: :create
 
+  default_scope -> { order(:url) }
+
+
   def full_name
     url.split("github.com/").last
   end
